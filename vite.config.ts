96c4e72f9544d2455b01e634744f00d242d5f998
Endijs Kirsteins <endijs.kirsteins@gmail.com>
2025-10-04 01:01:20 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/EndijsKirsteins234924.github.io/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
